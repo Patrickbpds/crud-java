5c2ce334ecfe57ce2b3ff0e06849eff6005a9728
Patrick <patrickbpds@gmail.com>
2025-05-30 17:16:03 -0300

    feat: add ResourceNotFoundExceptions for improved error handling in UserService

---
+++ src/main/java/com/patrick/crud/models/exceptions/ResourceNotFoundExceptions.java
@@ -0,0 +1,8 @@
+package com.patrick.crud.models.exceptions;
+
+public class ResourceNotFoundExceptions extends RuntimeException {
+
+    public ResourceNotFoundExceptions(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/patrick/crud/service/UserService.java
+++ src/main/java/com/patrick/crud/service/UserService.java
@@ -1,6 +1,7 @@
 package com.patrick.crud.service;
 
 import com.patrick.crud.mapper.UserMapper;
+import com.patrick.crud.models.exceptions.ResourceNotFoundExceptions;
 import com.patrick.crud.models.responses.UserResponse;
 import com.patrick.crud.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,9 @@ public class UserService {
 
     public UserResponse findById(final Long id) {
         return userMapper.fromEntity(
-                userRepository.findById(id).orElse(null)
+                userRepository.findById(id).orElseThrow(() ->
+                        new ResourceNotFoundExceptions("Object not found with id: " + id +
+                                " Type: "+ UserResponse.class.getSimpleName()))
         );
     }
 }
