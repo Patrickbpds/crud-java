fc226cd1a78965cccc2e87dc88b8adca1278a337
Patrick <patrickbpds@gmail.com>
2025-05-31 16:43:52 -0300

    fix: add constraint directly

--- src/main/resources/db/migration/V6__create_constraint_public_id.sql
+++ src/main/resources/db/migration/V6__create_constraint_public_id.sql
@@ -1,2 +1,11 @@
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_constraint
+        WHERE conname = 'public_id_unique'
+    ) THEN
 ALTER TABLE crud_tables.crud_users
-    ADD CONSTRAINT IF NOT EXISTS public_id_unique UNIQUE (public_id);
+    ADD CONSTRAINT public_id_unique UNIQUE (public_id);
+END IF;
+END$
