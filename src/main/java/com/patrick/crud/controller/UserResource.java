30243f2f095e58dc5cb36fdcca528757dc252d70
Patrick <patrickbpds@gmail.com>
2025-06-02 17:20:44 -0300

    feat: enhance UserResource with OpenAPI annotations for improved API documentation

--- src/main/java/com/patrick/crud/controller/UserResource.java
+++ src/main/java/com/patrick/crud/controller/UserResource.java
@@ -1,16 +1,42 @@
 package com.patrick.crud.controller;
 
+import com.patrick.crud.controller.exceptions.Error;
 import com.patrick.crud.models.responses.UserResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Tag(name="UserResource", description = "Resource responsible for user operations")
 @RequestMapping("/api/users")
 public interface UserResource {
 
+    @Operation(summary = "Find user by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User found"),
+            @ApiResponse(
+                    responseCode = "404", description = "User not found",
+                    content = @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = Error.class)
+                    )),
+            @ApiResponse(
+                    responseCode = "500", description = "Internal server error",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Error.class)
+                    )),
+
+    })
     @GetMapping("/{publicId}")
-    ResponseEntity<UserResponse> findById(@PathVariable(name="publicId") final Long publicId);
+    ResponseEntity<UserResponse> findById(
+            @PathVariable(name="publicId") final Long publicId);
 
 
 }
