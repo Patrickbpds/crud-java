dfa0e7eaa311fcc9c19cbb7a3f66465ffd4faed4
Patrick <patrickbpds@gmail.com>
2025-06-04 20:12:19 -0300

    fix: update ResourceNotfoundExceptions to extend ResponseStatusException and set HTTP status

--- src/main/java/com/patrick/crud/models/exceptions/ResourceNotFoundExceptions.java
+++ src/main/java/com/patrick/crud/models/exceptions/ResourceNotFoundExceptions.java
@@ -1,8 +1,13 @@
 package com.patrick.crud.models.exceptions;
 
-public class ResourceNotFoundExceptions extends RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundExceptions extends ResponseStatusException {
 
     public ResourceNotFoundExceptions(String message) {
-        super(message);
+        super(HttpStatus.NOT_FOUND, message);
     }
 }
