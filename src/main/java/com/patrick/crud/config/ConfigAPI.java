71fba5d400a315cb022a30711b8a4598653351cd
Patrick <patrickbpds@gmail.com>
2025-06-04 11:17:47 -0300

    docs: enhance openapi configuration with contact and license information

--- src/main/java/com/patrick/crud/config/ConfigAPI.java
+++ src/main/java/com/patrick/crud/config/ConfigAPI.java
@@ -1,7 +1,9 @@
 package com.patrick.crud.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +17,14 @@ public class ConfigAPI {
                 .info(new Info()
                         .title("CRUD API")
                         .version("1.0.0")
-                        .description("API component for CRUD operations developed by Patrick"));
+                        .description("API component for CRUD operations developed by Patrick")
+                        .contact(new Contact()
+                                .name("Patrick Batista")
+                                .email("patrickbpds@outlook.com")
+                                .url("https://github.com/Patrickbpds"))
+                        .license(new License()
+                                .name("GPL 3.0")
+                                .url("https://www.gnu.org/licenses/gpl-3.0"))
+                );
     }
 }
