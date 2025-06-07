91263ca4290c80b6180c4e474d2ce5e3923c025d
Patrick <patrickbpds@gmail.com>
2025-06-07 15:35:33 -0300

    refactor: add final modifier to parameter in findByPublicId method

--- src/main/java/com/patrick/crud/repository/UserRepository.java
+++ src/main/java/com/patrick/crud/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findByPublicId(String publicId);
+    Optional<User> findByPublicId(final String publicId);
 
     Optional<User> findByEmail(final String email);
 }
