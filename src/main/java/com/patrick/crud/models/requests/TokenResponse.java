0a08f1cc78f5aef3b122c6132e69d207f5b1cf06
Patrick <patrickbpds@gmail.com>
2025-06-11 19:56:21 -0300

    feat: add model for JWT token handling

---
+++ src/main/java/com/patrick/crud/models/requests/TokenResponse.java
@@ -0,0 +1,10 @@
+package com.patrick.crud.models.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+    private String token;
+}
