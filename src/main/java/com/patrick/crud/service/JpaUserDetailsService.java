74e0a947ae660fd21c74794d5b383899a518c910
Patrick <patrickbpds@gmail.com>
2025-06-11 20:00:19 -0300

    feat: implement service for user authentication

---
+++ src/main/java/com/patrick/crud/service/JpaUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.patrick.crud.service;
+
+import com.patrick.crud.repository.UserRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repo;
+
+    public JpaUserDetailsService(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repo.findByEmail(email)
+                .map(user -> org.springframework.security.core.userdetails.User
+                        .withUsername(user.getEmail())
+                        .password(user.getPassword())
+                        .authorities("ROLE_USER")
+                        .build())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
