d960a9eb79cc2c31661fd333451c3964b29675b5
Patrick <patrickbpds@gmail.com>
2025-06-12 15:37:43 -0300

    feat: enhance jwt service with token validation and expiration checks

--- src/main/java/com/patrick/crud/service/JwtService.java
+++ src/main/java/com/patrick/crud/service/JwtService.java
@@ -2,20 +2,26 @@ package com.patrick.crud.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtService {
+
     @Value("${JWT_SECRET}")
     private String secret;
+
+    private final long jwtExpiration = 86400000;
 
     public String generateToken(String email) {
         return JWT.create()
                 .withSubject(email)
                 .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + jwtExpiration))
                 .sign(Algorithm.HMAC256(secret));
     }
 
@@ -24,5 +30,26 @@ public class JwtService {
                 .build()
                 .verify(token)
                 .getSubject();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        try {
+            final String username = extractEmail(token);
+            return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+        } catch (JWTVerificationException e) {
+            return false;
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+        try {
+            Date expiration = JWT.require(Algorithm.HMAC256(secret))
+                    .build()
+                    .verify(token)
+                    .getExpiresAt();
+            return expiration.before(new Date());
+        } catch (JWTVerificationException e) {
+            return true;
+        }
     }
 }
