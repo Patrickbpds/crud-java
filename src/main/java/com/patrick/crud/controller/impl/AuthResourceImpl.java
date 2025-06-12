3db7fcea2baee1c762228094027b9024aa46c8cf
Patrick <patrickbpds@gmail.com>
2025-06-12 13:09:52 -0300

    feat: implement authentication resource with login and registration endpoints

---
+++ src/main/java/com/patrick/crud/controller/impl/AuthResourceImpl.java
@@ -0,0 +1,31 @@
+package com.patrick.crud.controller.impl;
+
+import com.patrick.crud.controller.AuthResource;
+import com.patrick.crud.models.requests.CreateUserRequest;
+import com.patrick.crud.models.requests.LoginRequest;
+import com.patrick.crud.models.requests.TokenResponse;
+import com.patrick.crud.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class AuthResourceImpl implements AuthResource {
+
+    private final AuthService AuthService;
+
+    @Override
+    public ResponseEntity<TokenResponse> login(@Valid LoginRequest loginRequest) {
+        TokenResponse token = AuthService.login(loginRequest);
+        return ResponseEntity.ok(token);
+    }
+
+    @Override
+    public ResponseEntity<Void> register(@Valid CreateUserRequest createUserRequest){
+        AuthService.register(createUserRequest);
+        return ResponseEntity.status(201).build();
+    }
+
+}
