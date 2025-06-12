876f5d6cf113e16e3ff6061e11a2fc91c3637256
Patrick <patrickbpds@gmail.com>
2025-06-12 13:11:57 -0300

    refactor: update to use record and lombok

--- src/main/java/com/patrick/crud/models/requests/LoginRequest.java
+++ src/main/java/com/patrick/crud/models/requests/LoginRequest.java
@@ -3,19 +3,17 @@ package com.patrick.crud.models.requests;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.With;
 
-@Getter
-@Setter
-public class LoginRequest {
+@With
+public record LoginRequest (
 
     @NotBlank(message = "Email required")
     @Email(message = "Invalid email format")
     @Schema(example = "patrick@example.com")
-    private String email;
+    String email,
 
     @NotBlank(message = "Password required")
     @Schema(example = "123456")
-    private String password;
-}
+    String password
+){}
