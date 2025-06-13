7ffab461fe12e42a7e7b853dc2f3b1168e76704f
Patrick <patrickbpds@gmail.com>
2025-06-13 07:05:44 -0300

    fix: update tag name

--- src/main/java/com/patrick/crud/controller/AuthResource.java
+++ src/main/java/com/patrick/crud/controller/AuthResource.java
@@ -16,7 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "AuthController", description = "Authentication Endpoints")
+@Tag(name = "AuthResource", description = "Authentication Endpoints")
 @RequestMapping("/auth")
 public interface AuthResource {
 
