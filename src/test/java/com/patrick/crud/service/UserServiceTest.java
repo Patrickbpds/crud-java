59476fb4c2ff85306c53c13c782f9ee92c2252a5
Patrick Batista <patrickbpds@gmail.com>
2025-06-23 15:10:02 -0300

    test: add test to find all method

--- src/test/java/com/patrick/crud/service/UserServiceTest.java
+++ src/test/java/com/patrick/crud/service/UserServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -69,5 +70,26 @@ class UserServiceTest {
 
         verify(userRepository, times(1)).findByPublicId("123456");
         verify(userMapper, times(0)).fromEntity(any(User.class));
+    }
+
+    @Test
+    void whenCallFindAll_thenReturnListOfUserResponse() {
+        // Given
+        User mockUser = new User();
+        UserResponse mockResponse = mock(UserResponse.class);
+
+        when(userRepository.findAll()).thenReturn(List.of(mockUser, mockUser));
+        when(userMapper.fromEntity(any(User.class))).thenReturn(mockResponse);
+
+        // When
+        List<UserResponse> result = userService.findAll();
+
+        // Then
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(2, result.size());
+        Assertions.assertEquals(UserResponse.class, result.get(0).getClass());
+
+        verify(userRepository, times(1)).findAll();
+        verify(userMapper, times(2)).fromEntity(any(User.class));
     }
 }
